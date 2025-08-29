66e9334af7592c5fa8bc2758753516b5d50d9762
ARUN KUMAR.A <86552741+Arun12kumar@users.noreply.github.com>
2025-08-29 17:16:57 +0530

    commit

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     password:{type:String, required: true},
     role: {
     type: String,
-    enum: ["admin", "manage"], // allowed values
+    enum: ["admin", "editor"], // allowed values
     default: "admin", // default role
     },
     verifyotp:{type:String, default: '',max: 6},
