b83d6501444edc47af34a7569cf3e32821d6f5fe
ARUN KUMAR.A <86552741+Arun12kumar@users.noreply.github.com>
2025-09-01 15:08:14 +0530

    commit

---
+++ src/models/ProductVariant.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const productVarientSchema = new mongoose.Schema(
+  {
+    productvarient_name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product", 
+        required: true,
+    },
+    productvarient_description: {
+      type: String,
+      trim: true,
+    },
+    productvarient_price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    productvarient_images: {
+      type: [String], // array of image URLs
+      validate: {
+        validator: function (val) {
+          return val.length >= 4 && val.length <= 6;
+        },
+        message: "A product variant must have between 4 and 6 images.",
+      },
+      required: true,
+    },
+    sale_price: {
+      type: Number,
+      min: 0,
+      default: null,
+    },
+    productvarient_stock_quantity: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const productVarientModel = mongoose.model(
+  "Product_Varient",
+  productVarientSchema
+);
+
+export default productVarientModel;
---
+++ src/models/categoryModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema(
+  {
+    category_name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    category_imageUrl:{type:String,required:true},
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const CategoryModel = mongoose.model("Category", categorySchema);
+
+export default CategoryModel;
